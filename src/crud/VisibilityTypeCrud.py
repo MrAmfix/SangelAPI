1bf0bc491b3d96ce09006165fbb796a81a9e726c
MrAmfix <nat.rud.ufa@gmail.com>
2024-10-01 22:19:48 +0300

    [feat]: Added base cruds

---
+++ src/crud/VisibilityTypeCrud.py
@@ -0,0 +1,14 @@
+from src.crud.base.factory import BaseCrudFactory
+from src.models.VisibilityType import VisibilityType
+from src.schemas.system_schemas import VisibilityTypeUpdate, VisibilityTypeCreate, VisibilityTypeGet
+
+
+class VisibilityTypeCrud(
+    BaseCrudFactory(
+        model=VisibilityType,
+        update_schema=VisibilityTypeUpdate,
+        create_schema=VisibilityTypeCreate,
+        get_schema=VisibilityTypeGet
+    )
+):
+    pass
