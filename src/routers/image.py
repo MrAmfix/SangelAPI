696621ae9aec6993969b6a1a996382f0e3fede22
Apekun Dmitry <zasferg@gmail.com>
2024-11-21 18:32:13 +0300

    [fix]: fixed request comments, added packeges to requirements.

--- src/routers/image.py
+++ src/routers/image.py
@@ -3,17 +3,17 @@ import io
 from fastapi import File, UploadFile, HTTPException, status
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi import APIRouter
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from settings import MAX_FILE_SIZE, UPLOAD_FOLDER
 from utils.image import image_name_rename
 
 
 
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter(prefix="/api")
 
 
-@router.post("/upload", summary="Загрузка фотографии")
+@router.post("/client/upload_photo", summary="Загрузка фотографии")
 async def uploag_image(
     file: UploadFile = File(...),
 ):
@@ -33,23 +33,16 @@ async def uploag_image(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="File size exceeds the maximum limit of 8MB.",
         )
-
     try:
         contents = await file.read()
-        with Image.open(io.BytesIO(contents)) as image:
-            image.verify()
         with Image.open(io.BytesIO(contents)) as img:
             if img.format not in ["JPEG", "PNG"]:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
                     detail="Invalid image format. Only JPEG and PNG are allowed.",
-                )
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="File is not a valid image."
-        )
+                ) 
+            img.verify()
 
-    try:
         file.filename = image_name_rename()
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         with open(file_path, "wb") as buffer:
@@ -60,29 +53,31 @@ async def uploag_image(
         #
 
         return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"message": f"File successfully saved as {file_path}."},
+                status_code=status.HTTP_200_OK,
+                content={"message": f"File successfully saved as {file_path}."},
+            )
+        
+    except UnidentifiedImageError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="File is not a valid image."
+        )
+    except FileNotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error: The directory '{UPLOAD_FOLDER}' does not exist.",
+        )
+    except IOError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"IOError occurred: {str(e)}",
+        )
+    except OSError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"OS error occurred: {str(e)}",
         )
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An error occurred: {str(e)}",
         )
-
-
-@router.get("/images/{filename}")
-async def get_image(filename: str):
-    file_path = os.path.join(UPLOAD_FOLDER, filename)
-    if not os.path.exists(file_path):
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Image not found."
-        )
-
-    return FileResponse(
-        file_path,
-        media_type=(
-            "image/jpeg"
-            if filename.lower().endswith(".jpg") or filename.lower().endswith(".jpeg")
-            else "image/png"
-        ),
-    )
