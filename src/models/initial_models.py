61a5ce0f5a25eb2829ce0947f3e0d6252d056e89
MrAmfix <nat.rud.ufa@gmail.com>
2024-12-02 17:54:00 +0300

    [feat]: Added alembic instruction and cropped models

--- src/models/initial_models.py
+++ src/models/initial_models.py
@@ -1,10 +1,10 @@
 import uuid
-from typing import List, Optional
-from sqlalchemy import String, DateTime, Boolean, Text, Numeric, ForeignKey, CheckConstraint, UniqueConstraint
+from typing import Optional
+from sqlalchemy import String, DateTime, Boolean, Text, Numeric, CheckConstraint, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -29,63 +29,6 @@ class User(Base, AsyncAttrs):
     phone: Mapped[str] = mapped_column(String(15), unique=True, nullable=False, index=True)
     email: Mapped[Optional[str]] = mapped_column(String(255), unique=True, nullable=True, index=True)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
-    photo_id: Mapped[Optional[uuid.UUID]] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('media.id', ondelete='SET NULL'),
-        nullable=True
-    )
-    photo: Mapped["Media"] = relationship(
-        'Media',
-        back_populates='user',
-        lazy='selectin'
-    )
-
-    visibility_type_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('visibility_types.id'),
-        nullable=False
-    )
-    visibility_type: Mapped["VisibilityType"] = relationship(
-        'VisibilityType',
-        back_populates='users',
-        lazy='selectin'
-    )
-
-    tokens: Mapped[List["Token"]] = relationship(
-        'Token',
-        back_populates='user',
-        lazy='selectin'
-    )
-    devices: Mapped[List["UserDevice"]] = relationship(
-        'UserDevice',
-        back_populates='user',
-        lazy='selectin'
-    )
-    favourite_contacts: Mapped[List["FavouriteContact"]] = relationship(
-        'FavouriteContact',
-        back_populates='owner',
-        lazy='selectin'
-    )
-    linked_contacts: Mapped[List["FavouriteContact"]] = relationship(
-        'FavouriteContact',
-        back_populates='linked_user',
-        lazy='selectin'
-    )
-    notifications: Mapped[List["Notification"]] = relationship(
-        'Notification',
-        back_populates='user',
-        lazy='selectin'
-    )
-    events: Mapped[List["Event"]] = relationship(
-        'Event',
-        back_populates='user',
-        lazy='selectin'
-    )
-    observers: Mapped[List["Observer"]] = relationship(
-        'Observer',
-        back_populates='user',
-        lazy='selectin'
-    )
 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
@@ -98,17 +41,6 @@ class Media(Base, AsyncAttrs):
                                           default=uuid.uuid4)
     media_link: Mapped[str] = mapped_column(String(255), nullable=False)
     is_photo: Mapped[bool] = mapped_column(Boolean, nullable=False)
-
-    user_id: Mapped[Optional[uuid.UUID]] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id'),
-        nullable=False
-    )
-    user: Mapped["User"] = relationship(
-        'User',
-        back_populates='photo',
-        lazy='selectin'
-    )
 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
@@ -125,7 +57,6 @@ class VerificationCode(Base, AsyncAttrs):
                                           default=uuid.uuid4)
     code: Mapped[str] = mapped_column(String(6), nullable=False)
     phone: Mapped[str] = mapped_column(String(15), nullable=False)
-
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
 
@@ -135,17 +66,6 @@ class Token(Base, AsyncAttrs):
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True,
                                           default=uuid.uuid4)
     refresh_token: Mapped[str] = mapped_column(Text, nullable=False, unique=True, index=True)
-
-    user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id'),
-        nullable=False
-    )
-    user: Mapped["User"] = relationship(
-        'User',
-        back_populates='tokens',
-        lazy='selectin'
-    )
 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
@@ -157,13 +77,6 @@ class DeviceProduct(Base, AsyncAttrs):
                                           default=uuid.uuid4)
     mac_address: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, index=True)
     description: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
-
-    users: Mapped[List["UserDevice"]] = relationship(
-        'UserDevice',
-        back_populates='device',
-        lazy='selectin'
-    )
-
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
 
@@ -173,66 +86,20 @@ class UserDevice(Base, AsyncAttrs):
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True,
                                           default=uuid.uuid4)
 
-    user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id'),
-        nullable=False
-    )
-    user: Mapped["User"] = relationship(
-        'User',
-        back_populates='devices',
-        lazy='selectin'
-    )
-
-    device_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('device_products.id'),
-        nullable=False
-    )
-    device: Mapped["DeviceProduct"] = relationship(
-        'DeviceProduct',
-        back_populates='users',
-        lazy='selectin'
-    )
-
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
 
 class FavouriteContact(Base, AsyncAttrs):
     __tablename__ = 'favourite_contacts'
     __table_args__ = (
-        UniqueConstraint("owner_id", "phone", name="unique_favourite_contact"),
         CheckConstraint("name ~ '^[А-Яа-я-]+$'", name="check_favourite_name_cyrillic"),
         CheckConstraint("phone ~ '^\\+7\\d{10}$' OR phone ~ '^8\\d{10}$'", name="check_favourite_phone_format"),
-        CheckConstraint("owner_id != linked_user_id", name="check_different_favourite_users")
     )
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True,
                                           default=uuid.uuid4)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
     phone: Mapped[str] = mapped_column(String(15), nullable=False)
-
-    owner_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id'),
-        nullable=False
-    )
-    owner: Mapped["User"] = relationship(
-        'User',
-        back_populates='favourite_contacts',
-        lazy='selectin'
-    )
-
-    linked_user_id: Mapped[Optional[uuid.UUID]] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id', ondelete='SET NULL'),
-        nullable=True
-    )
-    linked_user: Mapped["User"] = relationship(
-        'User',
-        back_populates='linked_contacts',
-        lazy='selectin'
-    )
 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
@@ -245,17 +112,6 @@ class Notification(Base, AsyncAttrs):
     title: Mapped[str] = mapped_column(Text, nullable=False)
     body: Mapped[str] = mapped_column(Text, nullable=False)
 
-    user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id'),
-        nullable=False
-    )
-    user: Mapped["User"] = relationship(
-        'User',
-        back_populates='notifications',
-        lazy='selectin'
-    )
-
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
 
@@ -265,12 +121,6 @@ class VisibilityType(Base, AsyncAttrs):
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True,
                                           default=uuid.uuid4)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
-
-    users: Mapped[List["User"]] = relationship(
-        'User',
-        back_populates='visibility_type',
-        lazy='selectin'
-    )
 
 
 class Event(Base, AsyncAttrs):
@@ -288,36 +138,6 @@ class Event(Base, AsyncAttrs):
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     complete_date: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
 
-    user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id'),
-        nullable=False
-    )
-    user: Mapped["User"] = relationship(
-        'User',
-        back_populates='events',
-        lazy='selectin'
-    )
-
-    # Есть поле security_group, но пока закомментировал, так как нет таблицы для гбр
-    # TODO
-    # security_group_id: Mapped[Optional[uuid.UUID]] = mapped_column(
-    #     UUID(as_uuid=True),
-    #     ForeignKey('security_groups.id', ondelete='SET NULL'),
-    #     nullable=True
-    # )
-    # security_group: Mapped["SecurityGroup"] = relationship(
-    #     'SecurityGroup',
-    #     back_populates='events',
-    #     lazy='selectin'
-    # )
-
-    observers: Mapped[List["Observer"]] = relationship(
-        'Observer',
-        back_populates='event',
-        lazy='selectin',
-    )
-
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
 
@@ -326,27 +146,5 @@ class Observer(Base, AsyncAttrs):
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True,
                                           default=uuid.uuid4)
-
-    user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('users.id'),
-        nullable=False
-    )
-    user: Mapped["User"] = relationship(
-        'User',
-        back_populates='observers',
-        lazy='selectin'
-    )
-
-    event_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey('events.id'),
-        nullable=False
-    )
-    event: Mapped["Event"] = relationship(
-        'Event',
-        back_populates='observers',
-        lazy='selectin'
-    )
 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
