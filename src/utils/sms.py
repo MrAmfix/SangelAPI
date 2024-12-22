019ea6a0f4e12de9d4f677a7f2cd86c27fca0902
Apekun Dmitry <zasferg@gmail.com>
2024-12-22 12:36:13 +0300

    [feat]:added sms sender

--- src/utils/sms.py
+++ src/utils/sms.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 from random import randint
 from src.settings import VERIFICATION_CODE_EXPIRE_SECONDS
 from src.utils.moscow_datetime import set_moscow_timezone, datetime_now_moscow
-
+import aiohttp
+from src.settings import SMS_API_PHONE_NUMBER, SMS_API_VERIFICATION_TOKEN
 
 def generate_code():
     return str(randint(1000000, 9999999))[1:]
@@ -14,3 +15,24 @@ def check_expired_code(created_date: datetime):
     """
     return (set_moscow_timezone(created_date) < datetime_now_moscow()
             - timedelta(seconds=VERIFICATION_CODE_EXPIRE_SECONDS))
+
+
+async def send_sms(destination: str):
+
+    url = "https://api.exolve.ru/messaging/v1/SendSMS"
+    headers = {
+        "Authorization": f"Bearer {SMS_API_VERIFICATION_TOKEN}"
+    }
+    data = {
+        "number": SMS_API_PHONE_NUMBER,
+        "destination": destination,
+        "text": generate_code(),
+    }
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url=url, headers=headers, json=data) as response:
+                return response
+        except Exception as e:
+            raise Exception(f"Произошла ошибка: {e}")
+
