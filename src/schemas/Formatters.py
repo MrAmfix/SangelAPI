8c0235f454dfd82f2c833626f344d57d3662205f
MrAmfix <nat.rud.ufa@gmail.com>
2024-12-28 01:02:41 +0300

    [fix]: Fixed classmethod in PhoneRequest

--- src/schemas/Formatters.py
+++ src/schemas/Formatters.py
@@ -5,6 +5,7 @@ import re
 class PhoneRequest(BaseModel):
     phone: str = Field(..., description="Phone number")
 
+    @classmethod
     @field_validator("phone", mode="before")
-    def normalize_phone(self, v):
+    def normalize_phone(cls, v):
         return re.sub(r"^8(\d{10})$", r"+7\1", v)
