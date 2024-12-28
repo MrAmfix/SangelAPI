06e78913d258b4e200c77de7ee10f230a3124240
MrAmfix <nat.rud.ufa@gmail.com>
2024-12-28 14:26:59 +0300

    [fix]: Fixed logger

--- src/utils/loggers.py
+++ src/utils/loggers.py
@@ -1,22 +1,8 @@
 import logging
 import inspect
-import colorlog
 from functools import wraps
 
-
-formatter = colorlog.ColoredFormatter(
-    "%(log_color)s%(levelname)s | %(message)s",
-    log_colors={
-        "DEBUG": "blue",
-        "INFO": "blue",
-        "WARNING": "yellow",
-        "ERROR": "red",
-        "CRITICAL": "bold_red",
-    },
-)
-
 handler = logging.StreamHandler()
-handler.setFormatter(formatter)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
@@ -28,14 +14,18 @@ def api_logs(handler):
         bound_arguments = inspect.signature(handler).bind(*args, **kwargs).arguments
         params = {key: value for key, value in bound_arguments.items() if key != 'session'}
         log_text = f"Handler: {handler.__name__} | Params: {params}"
+        error_occurred = False
+
         try:
             res = await handler(*args, **kwargs)
-            logger.info(f"[INFO] | {log_text}")
             return res
         except Exception as e:
-            logger.error(f"[ERROR] | {log_text} | Exception: {str(e)}")
+            error_occurred = True
+            logger.error(f"{log_text} | Exception: {str(e)}")
             raise
         finally:
+            if not error_occurred:
+                logger.info(log_text)
             logger.debug("---------------------------")
 
     return wrapper
