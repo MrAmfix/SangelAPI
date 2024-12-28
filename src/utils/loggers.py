df5eac7f8e7fd9050538d3998ab8f57fcda32c3b
MrAmfix <nat.rud.ufa@gmail.com>
2024-12-28 14:43:31 +0300

    [fix]: Fixed incorrect logging

--- src/utils/loggers.py
+++ src/utils/loggers.py
@@ -2,10 +2,9 @@ import logging
 import inspect
 from functools import wraps
 
-handler = logging.StreamHandler()
+
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
 
 
 def api_logs(handler):
@@ -14,18 +13,15 @@ def api_logs(handler):
         bound_arguments = inspect.signature(handler).bind(*args, **kwargs).arguments
         params = {key: value for key, value in bound_arguments.items() if key != 'session'}
         log_text = f"Handler: {handler.__name__} | Params: {params}"
-        error_occurred = False
 
         try:
             res = await handler(*args, **kwargs)
-            return res
         except Exception as e:
-            error_occurred = True
             logger.error(f"{log_text} | Exception: {str(e)}")
             raise
         finally:
-            if not error_occurred:
-                logger.info(log_text)
+            logger.info(log_text)
             logger.debug("---------------------------")
+        return res
 
     return wrapper
