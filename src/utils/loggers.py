ac42921100ae325ce3025ba032785fcc261e63bc
MrAmfix <nat.rud.ufa@gmail.com>
2024-12-28 15:37:39 +0300

    [fix]: Final fixed logger

--- src/utils/loggers.py
+++ src/utils/loggers.py
@@ -4,8 +4,17 @@ from functools import wraps
 from colorlog import ColoredFormatter
 
 
+class CustomColoredFormatter(ColoredFormatter):
+    def format(self, record):
+        if record.levelname == "DEBUG":
+            self.log_colors["DEBUG"] = "white"
+            return f"{self.log_colors['DEBUG']}DEBUG:{self.reset} {record.getMessage()}"
+        else:
+            return super().format(record)
+
+
 handler = logging.StreamHandler()
-formatter = ColoredFormatter(
+formatter = CustomColoredFormatter(
     "%(log_color)s%(levelname)s:%(reset)s %(message)s",
     log_colors={
         "DEBUG": "white",
@@ -26,14 +35,14 @@ def api_logs(handler):
     async def wrapper(*args, **kwargs):
         bound_arguments = inspect.signature(handler).bind(*args, **kwargs).arguments
         params = {key: value for key, value in bound_arguments.items() if key != 'session'}
-        log_text = f"Handler: {handler.__name__} | Params: {params}"
+        log_text = f"     Handler: {handler.__name__} | Params: {params}"
 
         try:
             logger.debug('----------------------------')
             logger.info(log_text)
             return await handler(*args, **kwargs)
         except Exception as e:
-            logger.error(f"Exception: {str(e)}")
+            logger.error(f"     Exception: {str(e)}")
             raise
 
     return wrapper
