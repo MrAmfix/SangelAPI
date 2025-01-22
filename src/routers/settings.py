58bf2907d5e7b85c89760e0654ed1f62be760fe1
Kabachok <alexeych102@gmail.com>
2025-01-22 22:58:52 +0300

    [feat]: added set_visibility_type_handler

---
+++ src/routers/settings.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, Body
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.exceptions import HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST
+
+from src.auth.auth import access_token_auth
+from src.crud import UserCrud, VisibilityTypeCrud
+from src.database import get_session
+from src.utils.enums import DefaultVisibilityType
+from src.utils.loggers import api_logs
+
+
+settings = APIRouter(prefix='/settings')
+
+
+@settings.put('/set_visibility_type')
+@api_logs
+async def set_visibility_type_handler(
+        auth_data: dict = Depends(access_token_auth),
+        visibility_type: DefaultVisibilityType = Body(...),
+        session: AsyncSession = Depends(get_session)
+):
+    user_id = auth_data['user'].id
+    visibility_type_id = await VisibilityTypeCrud.get_by_enum(enum=visibility_type, session=session)
+
+    try:
+        await UserCrud.update(
+            visibility_type_id=visibility_type_id,
+            session=session,
+            record_id=user_id
+        )
+
+        return {'detail': 'Тип установлен'}
+    except IntegrityError as _ie:
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail=f'Неправильный формат аргументов'
+        )
+    except Exception as _e:
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail=str(_e)
+        )
