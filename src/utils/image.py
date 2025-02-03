64670b1ee0a6b96f8608605100cbfa30584a3fc8
Apekun Dmitry <zasferg@gmail.com>
2025-02-03 09:45:59 +0300

    [fix]:fixed comments

--- src/utils/image.py
+++ src/utils/image.py
@@ -1,24 +1,9 @@
 from typing import Optional
 from pydantic import UUID4
+from uuid import UUID
 
 
-def image_name_rename(user_id: str, rewrite: bool, img_name: Optional[str]) -> str:
-    try:
+def image_name_rename(user_id: UUID) -> str:
+        result = f"user_photo_{user_id}"
+        return result
 
-        if img_name is None:
-            index = 0
-            result = f"user_photo_{user_id}_{index}"
-            return result
-
-        if rewrite is False:
-            last_underscore_index = img_name.rfind("_")
-            number_part = img_name[last_underscore_index + 1 :]
-            if number_part.isdigit():
-                number = int(number_part)
-                incremented_number = number + 1
-                result = img_name[: last_underscore_index + 1] + str(incremented_number)
-                return result
-        return None
-
-    except Exception as e:
-        raise e
